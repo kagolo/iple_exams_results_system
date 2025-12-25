ebeda982701179750f95cf4969376a724c07eb4d
kagolo <84382333+kagolo@users.noreply.github.com>
2025-12-25 13:45:35 -0800

    Resumed django start

--- resultssys/resultssys/urls.py
+++ resultssys/resultssys/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 
-from  django.conf import settings
-from  django.conf.urls.static import  static
+# from  django.conf import settings
+# from  django.conf.urls.static import  static
 
 admin.site.site_header="IPLE EXAMS PROJECT"
 admin.site.index_title="IPLE EXAMS SYSTEM"
@@ -27,7 +27,7 @@ admin.site.site_title="LINK OF ISLAMIC SCHOOLS EXAMINATION SYSTEM"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('resultsapp.urls'))
+    # path('', include('resultsapp.urls'))
 ]
 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+# urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
